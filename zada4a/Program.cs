8621715199dc674d278d02aad194eea92cba1a85
User 12 <User 12@PC17>
2021-10-27 14:25:12 +0300

    Add project files.

---
+++ zada4a/Program.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+
+namespace zada4a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = input.Substring(input.IndexOf( Console.ReadLine()) + 1);
+            Console.WriteLine(output);
+        }
+    }
+}
